9ac68893e3c5f12c765b68ff8e0be7f97c5c4940
koredeycode <koredey4u@gmail.com>
2023-08-20 11:05:44 +0000

    color changes

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./Tile.css"; // You can create this CSS file for styling
 
 const valueToWordAndColor = {
-  0: { word: "Empty", color: "rgb(68, 155, 248)" },
-  2: { word: "Quasar", color: "rgb(48, 144, 247)" },
-  4: { word: "Nebula", color: "rgb(29, 134, 247)" },
-  8: { word: "Galaxy", color: "rgb(9, 123, 246)" },
-  16: { word: "Star", color: "rgb(8, 113, 226)" },
-  32: { word: "Planet", color: "rgb(8, 103, 207)" },
-  64: { word: "Solar", color: "rgb(7, 93, 187)" },
-  128: { word: "Comet", color: "rgb(6, 84, 167)" },
-  256: { word: "Asteroid", color: "rgb(5, 74, 148)" },
-  512: { word: "Meteor", color: "rgb(5, 64, 128)" },
-  1024: { word: "Orbit", color: "rgb(4, 54, 108)" },
-  2048: { word: "Black Hole", color: "rgb(3, 44, 89)" },
-  4096: { word: "Supernova", color: "rgb(3, 34, 69)" },
-  8192: { word: "Cosmic", color: "rgb(2, 25, 49)" },
-  16384: { word: "Eternity", color: "rgb(1, 15, 30)" },
+  0: { word: "Empty", color: "#f87666" },
+  2: { word: "Quasar", color: "#8c7051" },
+  4: { word: "Nebula", color: "#7b3e19" },
+  8: { word: "Galaxy", color: "#1d1e2c" },
+  16: { word: "Star", color: "#582b11" },
+  32: { word: "Planet", color: "#03012c" },
+  64: { word: "Solar", color: "#222e50" },
+  128: { word: "Comet", color: "#995d81" },
+  256: { word: "Asteroid", color: "#8c5383" },
+  512: { word: "Meteor", color: "#590004" },
+  1024: { word: "Orbit", color: "#6689a1" },
+  2048: { word: "Black Hole", color: "#51a3a3" },
+  4096: { word: "Supernova", color: "#5b9279" },
+  8192: { word: "Cosmic", color: "#23ce6b" },
+  16384: { word: "Eternity", color: "#177e89" },
 };
 
 const Tile = ({ value }) => {
